14271c2e6c2bbf64af28d7ba2c4cf4ecc4b365c4
douxxtech <douxx@douxx.tech>
2026-04-20 22:29:57 +0200

    space

--- articles/index.php
+++ articles/index.php
@@ -2,6 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Allow-Headers: Content-Type");
+
 function extractInfoFromMarkdown($fileContent)
 {
     $info = [
