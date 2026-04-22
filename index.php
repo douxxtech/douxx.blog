e8c0ce35f38a39b27c507a09a6d785e579930d1c
douxxtech <douxx@douxx.tech>
2026-04-22 22:03:04 +0200

    10

--- index.php
+++ index.php
@@ -73,13 +73,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/opendyslexic" rel="stylesheet">
 
-    <link href="assets/css/vars.css?9" rel="stylesheet">
-    <link href="assets/css/base.css?9" rel="stylesheet">
-    <link href="assets/css/layout.css?9" rel="stylesheet">
-    <link href="assets/css/content.css?9" rel="stylesheet">
-    <link href="assets/css/modes.css?9" rel="stylesheet">
-    <link href="assets/css/navigation.css?9" rel="stylesheet">
-    <link href="assets/css/responsive.css?9" rel="stylesheet">
+    <link href="assets/css/vars.css?10" rel="stylesheet">
+    <link href="assets/css/base.css?10" rel="stylesheet">
+    <link href="assets/css/layout.css?10" rel="stylesheet">
+    <link href="assets/css/content.css?10" rel="stylesheet">
+    <link href="assets/css/modes.css?10" rel="stylesheet">
+    <link href="assets/css/navigation.css?10" rel="stylesheet">
+    <link href="assets/css/responsive.css?10" rel="stylesheet">
 </head>
 
 <body>
@@ -127,14 +127,14 @@
         </main>
     </div>
 
-    <script src="assets/js/config.js?9"></script>
-    <script src="assets/js/renderer.js?9"></script>
-    <script src="assets/js/sidebar.js?9"></script>
-    <script src="assets/js/navigation.js?9"></script>
-    <script src="assets/js/progressbar.js?9"></script>
-    <script src="assets/js/loader.js?9"></script>
-    <script src="assets/js/toggles.js?9"></script>
-    <script src="assets/js/external-links.js?9"></script>
+    <script src="assets/js/config.js?10"></script>
+    <script src="assets/js/renderer.js?10"></script>
+    <script src="assets/js/sidebar.js?10"></script>
+    <script src="assets/js/navigation.js?10"></script>
+    <script src="assets/js/progressbar.js?10"></script>
+    <script src="assets/js/loader.js?10"></script>
+    <script src="assets/js/toggles.js?10"></script>
+    <script src="assets/js/external-links.js?10"></script>
 </body>
 
 </html>
